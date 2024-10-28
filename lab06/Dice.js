2c919b91d126813fd45f597105653425521031a2
NathalieKandil <kand0071@algonquinlive.com>
2024-10-28 15:20:28 -0400

    Update Dice.js

--- lab06/Dice.js
+++ lab06/Dice.js
@@ -1,13 +1,18 @@
 // Dice.js
 // This module enables the functionality for rolling dice in the Yatzy game.
 
+
 class Dice {
     constructor() {
-        this.value = 1;
+        this.value = 1; // Default value
+        this.held = false; // Indicates if the die is held
     }
 
     roll() {
-        this.value = Math.floor(Math.random() * 6) + 1;
+        if (!this.held) {
+            this.value = Math.floor(Math.random() * 6) + 1; // Roll the die
+        }
+        return this.value;
     }
 }
 
